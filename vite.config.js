d106eb514273c5b2645fed9c68c579bbed9167b4
Tim Lin <e0610k@gmail.com>
2022-12-12 01:16:10 +0800

    fix:vite.config drop console settings

--- vite.config.js
+++ vite.config.js
@@ -14,13 +14,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+  esbuild: {
+    drop: ['console', 'debugger'],
   },
 })
